e453a5d618d6a396a2b5e7747ed8da966d505e92
huyxayda <huxedu2299@gmail.com>
2025-02-22 17:40:10 +0800

    add change password function

--- crypto_function.py
+++ crypto_function.py
@@ -26,22 +26,46 @@ def check_exist_master_password():
         return False
 
 # create master password and save to a file
-def create_master_password(user_input):
+# mode 1 for first creating
+# mode 2 for changing password
+def create_master_password(user_input, mode):
     hash_value = gen_master_password_hash(user_input, fix_salt)
     # check if the file exist
     if check_exist_master_password():  #if file exist
         saved_pass_hash_value = get_passwd_from_file()
         if saved_pass_hash_value is not None:
-            print("Master password already existed")
+            if(mode == 1):
+                print("Master password already existed")
+            elif(mode == 2):
+                pass_key_pair = {'pass_hash': hash_value}
+                with open(pass_file, 'w') as file:
+                    json.dump(pass_key_pair, file) 
+                    print("Master password created successfully")
     else:
         # write hash value to a file
         pass_key_pair = {'pass_hash': hash_value}
         with open(pass_file, 'w') as file:
             json.dump(pass_key_pair, file) 
-            print("Mater password created successfully")
+            print("Master password created successfully")
+        print("Secret key is creating...")
+        create_secret_key(user_input,fix_salt, 1)
+        key = open_secret_key(user_input,fix_salt)
+        print("Your secret key is: ")
+        print(key)
 
 # change master password
-# def change_master_password():
+def change_master_password():
+    old_master_passwd = input("Please enter your old password: ")
+    while check_master_password(old_master_passwd) == False:
+        print("Your master password is not correct")
+        old_master_passwd = input("Please enter your old password: ")
+    new_master_passwd = input('Please enter your new master password: ')
+    create_master_password(new_master_passwd, 2)
+    print("Secret key is creating...")
+    create_secret_key(new_master_passwd,fix_salt, 2, old_master_passwd)
+    key = open_secret_key(new_master_passwd,fix_salt)
+    print("Your secret key is: ")
+    print(key)
 
 # read master password file             
 def get_passwd_from_file():
@@ -71,6 +95,29 @@ def gen_master_passwd_key(passwd,salt):
     key = base64.urlsafe_b64encode(kdf.derive(password))
     return key  # this key used for encrypted and decrypted secret key
 
+# open secret key from file
+def open_secret_key(master_passwd, salt):
+    passwd_cipher = Fernet(gen_master_passwd_key(master_passwd, salt))
+    with open(key_file, "r") as file:
+        content = json.load(file)
+        encrypted_key = bytes.fromhex(content.get('key')) #convert from hex to bytes
+        key = passwd_cipher.decrypt(encrypted_key)
+    return key
+
+# def create new secret key then save to a file
+# mode 1 for first creating
+# mode 2 for changing password
+def create_secret_key(master_passwd, salt, mode, old_passwd = ''):
+    passwd_cipher = Fernet(gen_master_passwd_key(master_passwd, salt))
+    if (mode == 1):
+        key = token_bytes(AES.block_size)
+    elif (mode == 2):
+        key = open_secret_key(old_passwd, salt)
+    key_data_encrypted = passwd_cipher.encrypt(key)
+    data = {'key': key_data_encrypted.hex()}    #save key as hex value
+    with open(key_file, "w") as file:
+        json.dump(data, file)
+
 # encrypt
 def encrypt_AES(key,iv, target_unencrypted_text):
     cipher = AES.new(key,AES.MODE_CBC,iv)
@@ -94,23 +141,11 @@ pass_file = Path(".\key\pass.txt")
 key = ''
 iv = token_bytes(AES.block_size)
 fix_salt = '274f2589a5002d3d8e8412c2a877729b' # hex
-passwd_cipher = Fernet(gen_master_passwd_key(master_passwd, fix_salt))
+passwd_cipher = ''
 
-if key_file.is_file():  #if file exist
-    with open(key_file, "r") as file:
-        content = json.load(file)
-        encrypted_key = bytes.fromhex(content.get('key')) #convert from hex to bytes
-        key = passwd_cipher.decrypt(encrypted_key)
-else: 
-    key = token_bytes(AES.block_size)
-    key_data_encrypted = passwd_cipher.encrypt(key)
-    data = {'key': key_data_encrypted.hex()}    #save key as hex value
-    with open(key_file, "w") as file:
-        json.dump(data, file)  
+# encrypted_text = encrypt_AES(key,iv, utf8_passwd)
+# decrypted_text = decrypt_AES(key,encrypted_text)
+# print( 'decrypted text: '+ decrypted_text)
 
-encrypted_text = encrypt_AES(key,iv, utf8_passwd)
-decrypted_text = decrypt_AES(key,encrypted_text)
-print( 'decrypted text: '+ decrypted_text)
-
-master_passwd_hash_value = gen_master_password_hash(master_passwd, fix_salt)
-print( 'maste passwd hash value: '+ master_passwd_hash_value)
+# master_passwd_hash_value = gen_master_password_hash(master_passwd, fix_salt)
+# print( 'maste passwd hash value: '+ master_passwd_hash_value)
--- menu.py
+++ menu.py
@@ -1,4 +1,4 @@
-from crypto_function import check_exist_master_password,check_master_password, create_master_password
+from crypto_function import change_master_password,check_exist_master_password,check_master_password, create_master_password
 
 
 def menu():
@@ -6,25 +6,29 @@ def menu():
     print(('-'*13) + 'Menu'+ ('-' *13))
     print('1. If you do not have master password. Choose this')
     print('2. Log in')
+    print('3. Change master password')
     print('Q. Exit')
     print('-'*30)
     return input(': ')
 
 def create_password():
     user_input = input('Please enter a master password: ')
-    create_master_password(user_input)
+    create_master_password(user_input, 1)
 
 def login():
     if (check_exist_master_password() == False):
         print('You do not have a master password, please create one.')
-        create_password
+        create_password()
     else:
         passwd = input('Please enter the master password: ')
         # check master password correct or not
         if (check_master_password(passwd)):
             print('Master password is correct!!!')
         else:
-            print('Master password is incorre')
+            print('Master password is incorrect')
+
+def menu_change_master_password():
+    change_master_password()
 
 choice = menu()
 while choice != 'Q':
@@ -32,6 +36,8 @@ while choice != 'Q':
         create_password()
     if choice == '2':
         login()
+    if choice == '3':
+        menu_change_master_password()
     else:
         choice = menu()
 exit()
